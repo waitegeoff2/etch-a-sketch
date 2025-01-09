b1f1490d16bd283c10783f663dae7c9db9796ad7
Geoff Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-01-09 10:06:00 -0500

    create grid, hover function and new grid button

--- java-etch.js
+++ java-etch.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
 const choosesize = document.querySelector(".sizebutton");
+const body = document.querySelector("body");
 
 
 
@@ -8,68 +9,77 @@ const choosesize = document.querySelector(".sizebutton");
 // create 16 divs within container which are columns, flex the container
 // within each column, create 16 divs, flex each column 
 
-// TRY THIS A DIFFERENT WAY
+//TRY REWRITING
+//CONTAINER - append n divs SIZE IT (FLEX DISPLAY)
+//append n rows to the divs SIZE IT
 
-// makeRow with 16
-// run function 16 times
-// flex that
+// <container>
+// <columns> FLEXING THESE
+//  <rows> these are below the columns so just go down
 
-// function makeRow () {
-//     for (i=0; i < 16; i++) {       
-//         const row = document.createElement("div");
-//         row.classList.add("gridrow");
-//         container.appendChild(row);
-//         row.textContent = "i";
-        
-//     }
-// };
 
 function makeRows (size) {
-    let screen = document.querySelector(".screen");
+
+    container.innerHTML = "";
+    
     for (i=0; i < size; i++) {
+
+        // for each loop creates a div column
+
         const column = document.createElement("div");
-        column.classList.add("gridcolumn");
+        column.classList.add("gridcolumn"); 
         for (j = 0; j < size; j++) {
+            // for each div column, loops SIZE rows and appends to the column
             const row = document.createElement("div");
             row.classList.add("gridrow");
             row.style.border = "1px solid black";
             column.appendChild(row);
+
+            //set the size of each div
+            row.style.height = (500/size) + "px";
+            row.style.width = (500/size) + "px";
             
+            // for each row div, adds the hover event
             row.addEventListener('mouseenter', () => {
                 row.style.backgroundColor = "grey";
             });
 
         }
-    screen.appendChild(column);
+    // appends everything to the container div once both loops are done
+    container.appendChild(column);
 }
 };
 
 makeRows(16);
 
-choosesize.addEventListener("click", () => {
+function newGrid() {
     let squaresides = prompt("How many squares per side (limit 100)?")
     
-    if (squaresides >= 100) {
-        makeRows(100);        
+    if (squaresides > 100) {
+        alert("Number cannot be higher than 100")       
     } else if (squaresides <= 100) {
         makeRows(squaresides);
-    }
-})
+    } 
+}
+
+// choosesize.addEventListener("click", () => {
+//     // delete the old container (ABOVE) and start a fresh one
+    
+    
+//     let squaresides = prompt("How many squares per side (limit 100)?")
+    
+//     if (squaresides > 100) {
+//         alert("Number cannot be higher than 100")       
+//     } else if (squaresides <= 100) {
+//         makeRows(squaresides);
+//     }
+// })
+
+choosesize.addEventListener("click", newGrid);
 
 
 
 
 
-// event listener for when mouse enters each gridrow div (MOUSE HOVER)
 
-// row.addEventListener("hover", ) {
-// turns black
-// } ???
-
-// CREATE INPUT (https://www.w3schools.com/html/html_forms.asp) for people to add "size" variable and pass that through
-// makerows function. Check out RPS for example
-
-// scissors.addEventListener("click", () => {
-//     playRound("scissors", "");
-//     });
 
